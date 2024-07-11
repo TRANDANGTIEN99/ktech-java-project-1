9a3598af731aaa256af94a429cd83681022812bb
TRANDANGTIEN99 <trandangtien10b1@gmail.com>
2024-07-11 10:22:12 +0900

    .

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,86 +1,97 @@
 package project;
-import com.oracle.svm.core.annotate.Delete;
-
-import javax.imageio.plugins.tiff.TIFFImageReadParam;
-import java.io.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        List<ToDo1> ToDo1List = readData();
-        System.out.println("Welcome!!!");
-        System.out.println("할일 없습니다");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-//        System.out.print("title: ");
-//        String title = reader.readLine();
-//        System.out.print("until: ");
-//        String until = reader.readLine();
-//        LocalDate untilDate = LocalDate.parse(until);
-
-
-            System.out.println("1. Create TODO ");
-            System.out.println("2. Edit TODO ");
-            System.out.println("3. Finish TODO ");
-            System.out.println("4. Delete TODO ");
-            System.out.println("5. Exit ");
-
+    private static final List<String> todos = new ArrayList<>();
+        public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Input: ");
-            String input = reader.readLine();
-            if (input.equals("5")) {
-                break;
-            } else if (input.equals("1")) {
-                System.out.print("title: ");
-                String title = reader.readLine();
-                System.out.print("until: ");
-                String until = reader.readLine();
-                LocalDate untilDate = LocalDate.parse(until);
-                ToDo1List.add(new ToDo1(
-                        title,
-                        untilDate
-
-                ));
+            while (true) {
+                printMenu();
+                int ints = Integer.parseInt(scanner.nextLine());
+                switch (ints) {
+                    case 1:
+                        createTodo(scanner);
+                        break;
+                    case 2:
+                        if (!todos.isEmpty()) {
+                            editTodo(scanner);
+                        } else {
+                            System.out.println("수정할 일 없습니다");
+                        }
+                        break;
+                    case 3:
+                        if (!todos.isEmpty()) {
+                            finishTodo();
+                        } else {
+                            System.out.println("할일 업습니다.");
+                        }
+                        break;
+                    case 4:
+                        if (!todos.isEmpty()) {
+                            deleteTodo(scanner);
+                        } else {
+                            System.out.println("삭제할 일 없습니다");
+                        }
+                        break;
+                    case 5:
+                        System.out.println("나간다");
+                        scanner.close();
+                        return;
+                    default:
+                        System.out.println("다시 입력하세요");
+                }
             }
-            System.out.println("Saved!!!");
         }
-
+ private static void printMenu() {
+     System.out.println("\n Welcome!");
+     if (todos.isEmpty()) {
+         System.out.println("할일없습니다!!!");
+     } else {
+         System.out.println("해야 할일은:");
+         for (int i = 0; i < todos.size(); i++) {
+             System.out.println((i + 1) + ": " + todos.get(i));
+         }
+     }
+     System.out.println("1. Create TODO ");
+     System.out.println("2. Edit TODO ");
+     System.out.println("3. Finish TODO ");
+     System.out.println("4. Delete TODO ");
+     System.out.println("5. Exit ");
+     System.out.print("Input: ");
+ }
+    private static void createTodo (Scanner scanner) {
+        System.out.print("할일 입력하세요: ");
+        String todo = scanner.nextLine();
+        todos.add(todo);
+        System.out.println("할일 입력성공.");
+    }
+    private static void editTodo (Scanner scanner) {
+        System.out.print("어떤 할일 수정하려고 합니까? ");
+        int index = Integer.parseInt(scanner.nextLine()) - 1;
+        if (index < 0 || index >= todos.size()) {
+            System.out.println("잘못 입력하셨습니다");
+            return;
+        }
+        System.out.print("새로 할일 입력: ");
+        String newTodo = scanner.nextLine();
+        todos.set(index, newTodo);
+        System.out.println("수정 완료.");
+    }
+    private static void finishTodo () {
+        System.out.println("첫번쩨 할일 완료");
+        todos.remove(0);
     }
 
-    private static List<ToDo1> readData() {
-        List<ToDo1> toDo1List = new ArrayList<>();
-        try (FileReader fileReader = new FileReader("todo.csv");
-             BufferedReader reader = new BufferedReader(fileReader)) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] values = line.split(",");
-                LocalDate untilDate = LocalDate.parse(values[1]);
-                toDo1List.add(new ToDo1(values[0], untilDate));
-            }
-        } catch (IOException e) {
-            System.out.println("에러 정보를 읽는다: " + e.getMessage());
+    private static void deleteTodo (Scanner scanner) {
+        System.out.print("할일 삭제합니까? ");
+        int index = Integer.parseInt(scanner.nextLine()) - 1;
+        if (index < 0 || index >= todos.size()) {
+            System.out.println("다시 입력하세요");
+            return;
         }
-        return toDo1List;
+        todos.remove(index);
+        System.out.println("삭제 완료");
     }
-
 }
-
-
-
-
-
-
-//       String Create = reader.readLine();
-//            String Edit = reader.readLine();
-//            String Finish= reader.readLine();
-//            String Delete= reader.readLine();
-//            ToDoList.add(new ToDo(
-//                    Create,
-//                    Edit,
-//                    Finish,
-//                    Delete
-//            ));
